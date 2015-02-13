285262f0d25a0e875bf86c0afdbe2cff84b2dad5
Aaron Fisher-Katz <afkatz@bu.edu>
2015-02-13 07:05:45 -0700

    removed stroop function and changed generation method for pairs

--- stroop.js
+++ stroop.js
@@ -2,12 +2,14 @@ var wait = true;
 var input = new Array();
 var pairs = new Array();
 var pairs_temp = new Array();
-var num_trials = 10;
+var num_trials = 15;
 var colors = [["red", "#FF0000"],
 			  ["green", "#00FF00"],
 			  ["blue", "#0000FF"],
 			  ["yellow", "#FFFF00"]];
 var len = colors.length;
+
+// generate word-color pairs
 for (var ii = 0; ii < len; ii++) {
 	for (var jj = 0; jj < len; jj++) {
 		if (ii != jj) {
@@ -21,10 +23,8 @@ var is_test = false;
 
 function KeyHandler() {
 	$(document).on('keydown', function(e) {
-		//wait = false;
-		//document.write(wait);
 		input[input.length] = String.fromCharCode(e.keyCode);
-		if (num_trials >= 0) {
+		if (num_trials > 1) {
 			next_stroop();
 			alert("ready for the next color?");
 		}
@@ -35,11 +35,12 @@ function KeyHandler() {
 
 $(document).ready(function() {
 	KeyHandler();
+	next_stroop();
 });
 
 
 function next_stroop() {
-	if (pairs_temp) {
+	if (pairs_temp.length > 0) {
 		var index = Math.floor(Math.random() * pairs_temp.length);
 		var display_word = "<font color=\"";
 		display_word += pairs_temp[index][1];
@@ -48,6 +49,9 @@ function next_stroop() {
 		display_word += "</font>";
 		$("#container").html(display_word);
 		pairs_temp.splice(index,1);
+		num_trials--;
+	} else {
+		pairs_temp = pairs.slice();
 	}
 
 }
